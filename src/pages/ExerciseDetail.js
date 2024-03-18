0b66551aef35b1f61fe6e4f8a40fc04cea83132f
Doobiii <Doobiii>
2024-03-18 11:33:50 +0530

    updated

---
+++ src/pages/ExerciseDetail.js
@@ -0,0 +1,66 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { Box } from "@mui/material";
+
+import { exerciseOptions, fetchData, youtubeOptions } from "../utils/FetchData";
+import Detail from "../components/Detail";
+import ExerciseVideos from "../components/ExerciseVideos";
+import SimilarExercises from "../components/SimilarExercises";
+
+const ExerciseDetail = () => {
+  const [ExerciseDetail, setExerciseDetail] = useState({});
+  const [exerciseVideos, setExerciseVideos] = useState([]);
+  const [targetMuscleExercises, settargetMuscleExercises] = useState([]);
+  const [equipmentExercises, setequipmentExercises] = useState([]);
+  const { id } = useParams();
+
+  useEffect(() => {
+    const fetchExercisesData = async () => {
+      const exerciseDbUrl = "https://exercisedb.p.rapidapi.com";
+      const youtubeSearchUrl =
+        "https://youtube-search-and-download.p.rapidapi.com";
+
+      const exerciseDetailData = await fetchData(
+        `${exerciseDbUrl}/exercises/exercise/${id}`,
+        exerciseOptions
+      );
+      setExerciseDetail(exerciseDetailData);
+      // console.log(exerciseDetailData);
+
+      const exerciseVideosData = await fetchData(
+        `${youtubeSearchUrl}/search?query=${exerciseDetailData.name} exercise`,
+        youtubeOptions
+      );
+      setExerciseVideos(exerciseVideosData.contents);
+
+      const targetMuscleExercisesData = await fetchData(
+        `${exerciseDbUrl}/exercises/target/${exerciseDetailData.target}`,
+        exerciseOptions
+      );
+      settargetMuscleExercises(targetMuscleExercisesData);
+
+      const equipmentExercisesData = await fetchData(
+        `${exerciseDbUrl}/exercises/equipment/${exerciseDetailData.equipment}`,
+        exerciseOptions
+      );
+      setequipmentExercises(equipmentExercisesData);
+    };
+    fetchExercisesData();
+  }, [id]);
+
+  return (
+    <Box>
+      <Detail ExerciseDetail={ExerciseDetail} />
+      <ExerciseVideos
+        exerciseVideos={exerciseVideos}
+        name={ExerciseDetail.name}
+      />
+      <SimilarExercises
+        targetMuscleExercises={targetMuscleExercises}
+        equipmentExercises={equipmentExercises}
+      />
+    </Box>
+  );
+};
+
+export default ExerciseDetail;
---
+++ src/pages/Home.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import { Box } from "@mui/material";
+import SearchExercises from "../components/SearchExercises";
+import Exercises from "../components/Exercises";
+import HeroBanner from "../components/HeroBanner";
+const Home = () => {
+  const [bodyPart, setBodyPart] = useState("all");
+  const [exercises, setExercises] = useState([]);
+  // console.log(bodyPart)
+  return (
+    <Box>
+      <HeroBanner />
+      <SearchExercises
+        setExercises={setExercises}
+        bodyPart={bodyPart}
+        setBodyPart={setBodyPart}
+      />
+      <Exercises
+        bodyPart={bodyPart}
+        exercises={exercises}
+        setExercises={setExercises}
+      />
+    </Box>
+  );
+};
+
+export default Home;
